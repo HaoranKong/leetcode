dfcb98a23408773ecde3d0655693ac5d661d1508
bupt-2917 <87065154+bupt-2917@users.noreply.github.com>
2024-04-14 21:09:10 +0800

    最长连续序列

--- code/src/main/java/最长连续序列/Solution2.java
+++ code/src/main/java/最长连续序列/Solution2.java
@@ -1,5 +1,11 @@
 package 最长连续序列;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * ClassName: Solution2
  * Package: 最长连续序列
@@ -10,5 +16,35 @@ package 最长连续序列;
  * @Version 1.0
  */
 public class Solution2 {
+    public int longestConsecutive(int[] nums) {
+        int longest=1;
+        int current=0;
+        Set<Integer>sets = new HashSet<Integer>();
+        for(int num:nums){
+            sets.add(num);
+        };
+        for(int num:sets)
+        {
+            if(!sets.contains(num-1))
+            {
+
+                while (sets.contains(num+1)){
+                    longest++;
+                    num++;
+                }
+
+            }
+            current=Integer.max(longest,current);
+            longest=1;
+
+        }
+        return current;
+
+    }
+    @Test
+    public void test(){
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
+        System.out.println(longestConsecutive(nums));
+    }
 
 }
