ca7bfb63b4b3bf25c821028b736a288754305aad
bupt-2917 <87065154+bupt-2917@users.noreply.github.com>
2024-04-14 20:33:05 +0800

    最长连续序列

--- code/src/main/java/最长连续序列/Solution.java
+++ code/src/main/java/最长连续序列/Solution.java
@@ -1,6 +1,11 @@
 package 最长连续序列;
 
-import java.util.Arrays;
+import org.junit.Test;
+
+import java.security.Key;
+import java.util.*;
+
+import static java.lang.Math.max;
 
 /**
  * ClassName: Solution
@@ -13,8 +18,51 @@ import java.util.Arrays;
  */
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
 
-    return 0;
+        if(nums.length==0)
+            return 0;
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<Integer,Integer>();
+        int flag=1;
+        int max=1;
+        Arrays.sort(nums);
+        for(int num:nums) {
+            if(!map.containsKey(num))
+                map.put(num,num);
+        }
+        Set<Integer> integers = map.keySet();
+
+        Integer[] num = integers.toArray(new Integer[0]);
+
+
+        if(num.length==1)
+            return 1;
+        for(int i=0;i<num.length-1;i++)
+        {
+            if(num[i]+1==num[i+1])
+
+            {
+                flag++;
+            }
+            else{
+                max=Integer.max(flag,max);
+                flag=1;
+
+            }
+
+
+
+        }
+        return Integer.max(flag,max);
+
     }
+  /**
+   *
+   * @author lyj
+   * @date 2024-04-14
+   */
+  @Test
+  public void test(){
+      int[] nums = {0,3,7,2,5,8,4,6,0,1};
+      System.out.println(longestConsecutive(nums));
+  }
 }
---
+++ code/src/main/java/最长连续序列/Solution2.java
@@ -0,0 +1,14 @@
+package 最长连续序列;
+
+/**
+ * ClassName: Solution2
+ * Package: 最长连续序列
+ * Description:
+ *
+ * @Author KHR
+ * @Create 2024/4/14 20:30
+ * @Version 1.0
+ */
+public class Solution2 {
+
+}
