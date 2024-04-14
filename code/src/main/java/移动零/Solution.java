ccdd413cd68600c6d8bf833e9e59bfdfcd9de4ae
bupt-2917 <87065154+bupt-2917@users.noreply.github.com>
2024-04-14 21:39:09 +0800

    移动零

---
+++ code/src/main/java/移动零/Solution.java
@@ -0,0 +1,57 @@
+package 移动零;
+
+import com.sun.org.apache.bcel.internal.generic.SWAP;
+import org.junit.Test;
+
+/**
+ * ClassName: Solution
+ * Package: 移动零
+ * Description:
+ *
+ * @Author KHR
+ * @Create 2024/4/14 21:16
+ * @Version 1.0
+ */
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int left=0;
+        int right=0;
+        for(int num:nums){
+
+            if(num!=0){
+                swap(nums,left,right);
+                left++;
+                right++;
+            }
+            else {
+                right++;
+            }
+        }
+
+
+    }
+
+
+
+    private void swap(int[] nums, int left, int right) {
+        int temp;
+        temp=nums[left];
+        nums[left]=right;
+        nums[right]=temp;
+    }
+
+    /**
+     *
+     * @author khr
+     * @date 2024-04-14
+     */
+    @Test
+    public void test(){
+       int[] nums = {0,1,0,3,12};
+       moveZeroes(nums);
+       for(int num:nums){
+           System.out.println(num);
+       }
+    }
+}
