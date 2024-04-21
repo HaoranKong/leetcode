6877cc63426f9def356d0d07adcd354afbde8b76
bupt-2917 <87065154+bupt-2917@users.noreply.github.com>
2024-04-21 15:30:10 +0800

    螺旋矩阵

--- code/src/main/java/螺旋矩阵/Solution.java
+++ code/src/main/java/螺旋矩阵/Solution.java
@@ -1,5 +1,7 @@
 package 螺旋矩阵;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,53 @@ import java.util.List;
  */
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-return new ArrayList<Integer>();
+        int top=0;
+        int right=matrix[0].length-1;
+        int bottom=matrix.length-1;
+        int left=0;
+        ArrayList<Integer> res=new ArrayList<Integer>();
+
+        while(top <= bottom && left <= right){
+            for(int i =left;i<=right;i++){
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for(int j=top;j<=bottom;j++){
+                res.add(matrix[j][right]);
+            }
+            right--;
+
+            if (top <= bottom){
+                for(int i=right;i>=left;i--){
+                    res.add(matrix[bottom][i]);
+                };
+                bottom--;
+            }
+
+            if (left <= right){
+                for(int j=bottom;j>=top;j--){
+                    res.add(matrix[j][left]);
+                }
+                left++;
+            }
+
+
+
+
+        }
+
+
+
+        return res;
+    }
+
+    /**
+     * @author khr
+     * @date 2024-04-21
+     */
+    @Test
+    public void test() {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7,8,9}};
+        System.out.println(spiralOrder(matrix));
     }
 }
