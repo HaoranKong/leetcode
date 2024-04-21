73f0c690910ad5f36ae0e0c9e8f2eebf06abd5d2
bupt-2917 <87065154+bupt-2917@users.noreply.github.com>
2024-04-21 17:25:03 +0800

    相交链表

--- code/src/main/java/相交链表/Solution.java
+++ code/src/main/java/相交链表/Solution.java
@@ -1,5 +1,8 @@
 package 相交链表;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * ClassName: Solution
  * Package: 相交链表
@@ -9,10 +12,25 @@ package 相交链表;
  * @Create 2024/4/21 17:08
  * @Version 1.0
  */
-/**
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
- public class Solution {
- public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
- }
- }
+
+
+        Set<ListNode> set=new HashSet<ListNode>();
+        while(headA!=null)
+        {
+            set.add(headA);
+            headA=headA.next;
+        }
+        while (headB!=null)
+        {
+            if(set.contains(headB)){
+                return headB;
+            }
+            headB=headB.next;
+        }
+        return null;
+    }
+}
